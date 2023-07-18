29f733dec039aaa3814f177f4a97cde90c6ccca7
saki8661 <saki8661@naver.com>
2023-07-18 16:42:46 +0900

    mall생성

---
+++ src/test/java/shop/mtcoding/mall2/Mall2ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.mall2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mall2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
